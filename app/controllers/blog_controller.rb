9ebf26380154798e28b0a4a96158d377f2d5f23a
emedosy <nosirve2011@gmail.com>
2017-03-06 19:17:28 +0000

    Blog index now has last post as feature and n-1..in the next ones

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,6 +1,7 @@
 class BlogController < ApplicationController
     def index
         @posts = Post.all.limit(6).order("created_at desc").offset(1)
+        @first_post = Post.all.limit(1).order("created_at desc").first
         @article = Article.all.limit(3).order("created_at desc")
     end
 end
