28aba3d0424dc0c014375727b908e2dd3f5f4925
emedosy <nosirve2011@gmail.com>
2017-03-09 22:41:57 +0000

    Add dynamic articles to homepage

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
     validates :title, presence: true, length: {minimum: 5}
     validates :body, presence: true
-    has_attached_file :pic, styles: { large: "851x351!", medium: "330x230!", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :pic, styles: { large: "851x351!", medium: "330x180!", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :pic, content_type: /\Aimage\/.*\z/
 end
